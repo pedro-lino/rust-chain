be712809b203e9da4dae64eb8c3b5ba61ac05a75
Pedro Lino <pmdbl@hotmail.com>
2025-10-13 19:17:41 +0100

    Create basic RPC server

---
+++ node/src/main.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use jsonrpsee::{Extensions, RpcModule, server::ServerBuilder, types::ErrorObjectOwned};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let ctx = ();
+
+    let server = ServerBuilder::default().build("127.0.0.1:8080").await?;
+
+    let mut module: RpcModule<()> = RpcModule::new(ctx);
+
+    module.register_method(
+        "add",
+        |params, _ctx: &(), _ext: &Extensions| -> Result<u64, ErrorObjectOwned> {
+            let (a, b): (u64, u64) = params.parse()?;
+            Ok(a + b)
+        },
+    )?;
+
+    let handle = server.start(module);
+    println!("JSON-RPC on http://127.0.0.1:8080");
+    handle.stopped().await;
+    Ok(())
+}
